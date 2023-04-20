e9eab096f8d5c350387769d97bf484d4759fb22f
Swyzo <88983705+Swyzo@users.noreply.github.com>
2023-04-20 16:44:47 -0700

    Add files via upload
    This version implemented a keyboard interrupt to go from the work timer to the rest timer. However, the keystrokes to interrupt are CTRL+C or CTRL+Z which isn't particularly user friendly

--- pomodoro.py
+++ pomodoro.py
@@ -1,18 +1,37 @@
 import time
 import os
+from readchar import readkey, readchar, key
 
 def timeconvert (time):
     return time*60
 
+#This technically behaves as I wanted it to but it needs CTRL+C/Z to quit which isn't user friendly. 
 def countdown(timegiven, label):
+    try:
+        print("Press CTRL+C or CTRL+Z to Exit")
+        while timegiven:
+            #time // 60
+            #time % 60
+            minute, second = divmod(timegiven, 60)
+            print(f"{label}: {minute:02d}:{second:02d}", end="\r")
+            time.sleep(1)
+            timegiven -= 1
+    except KeyboardInterrupt:
+        pass
+
     #60
-    while timegiven:
-        #time // 60
-        #time % 60
-        minute, second = divmod(timegiven, 60)
-        print(f"{label}: {minute:02d}:{second:02d}", end="\r")
-        time.sleep(1)
-        timegiven -= 1
+#    while True:
+#        print("Press Any Key to Exit")
+#        while timegiven:
+            #time // 60
+            #time % 60
+#            minute, second = divmod(timegiven, 60)
+#            print(f"{label}: {minute:02d}:{second:02d}", end="\r")
+#            time.sleep(1)
+#            timegiven -= 1
+#        exitKey = readchar()
+#        if exitKey == key.ENTER:
+#            break
 
 def pomodoro(work, rest):
     #convert min to sec
@@ -25,4 +44,5 @@ def pomodoro(work, rest):
 
 work = int(input("Enter work time (min): "))
 rest = int(input("Enter rest time (min): "))
+os.system("clear||cls")
 pomodoro(work, rest)
